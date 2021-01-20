0216c1fcf9855ef323d85aad02bd125f9e0b5521
Kaosz Elemer <attila.k84@gmail.com>
2021-01-20 06:41:27 +0100

    trying to implement movement rules

--- Cell.lua
+++ Cell.lua
@@ -8,10 +8,36 @@ function Cell:init(x, y, isWalkable, quad)
 end
 
 function Cell:click()
-        if selectedChar.isInStepState and not self.isOccupied then
-            
 
-            selectedChar:move(self.x, self.y)
+       --valami itt még nem jó meg kell nézni az OOP előtti szabályt a mozgáshoz
+        if selectedChar.isInStepState and not self.isOccupied then
+            if self.x + 1 < 11 and selectedChar.isWalkable[boardGrid[(self.x + 1)][self.y].class.name] and not boardGrid[(self.x + 1)][self.y].isOccupied then
+                selectedChar:move(self.x + 1, self.y)
+            
+            elseif self.x - 1 > 0 and selectedChar.isWalkable[boardGrid[self.x - 1][self.y].class.name] and not boardGrid[self.x - 1][self.y].isOccupied then 
+                selectedChar:move(self.x - 1, self.y)
+            
+            elseif self.y + 1 < 11 and selectedChar.isWalkable[boardGrid[(self.x)][self.y + 1].class.name] and not boardGrid[self.x][(self.y + 1)].isOccupied then 
+                selectedChar:move(self.x, self.y + 1)
+            
+            elseif self.y - 1 > 0 and selectedChar.isWalkable[boardGrid[self.x][self.y - 1].class.name] and not boardGrid[self.x][(self.y - 1)].isOccupied then 
+                selectedChar:move(self.x, self.y - 1)
+            
+            elseif self.x + 1 < 11 and self.y + 1 < 11 and selectedChar.isWalkable[boardGrid[(self.x + 1)][self.y + 1].class.name] and not boardGrid[(self.x + 1)][self.y + 1].isOccupied  then
+                selectedChar:move(self.x + 1, self.y + 1) 
+            
+            elseif self.x - 1 > 0 and self.y - 1 > 0 and selectedChar.isWalkable[boardGrid[(self.x - 1)][self.y - 1].class.name]  and not boardGrid[(self.x - 1)][self.y - 1].isOccupied then
+                selectedChar:move(self.x - 1, self.y - 1)
+               
+            elseif self.x + 1 < 11 and self.y - 1 > 0 and selectedChar.isWalkable[boardGrid[(self.x + 1)][self.y - 1].class.name] and not boardGrid[(self.x + 1)][self.y - 1].isOccupied then 
+                selectedChar:move(self.x + 1, self.y - 1)
+            
+            elseif self.x - 1 > 0 and self.y + 1 < 11 and selectedChar.isWalkable[boardGrid[(self.x - 1)][self.y + 1].class.name] and not boardGrid[(self.x - 1)][self.y + 1].isOccupied then 
+                selectedChar:move(self.x - 1, self.y + 1)
+            end
+            
+            -- ide kell hogy ha tobb a cell.x-em mint 2 akkor deszelektál
+
             board:resetAllCharacterStates(playerOne, playerTwo)
         end
 
