8acd4683b884437660def032e0c951354021a6cb
Kaosz Elemer <attila.k84@gmail.com>
2021-01-31 07:48:11 +0100

    alchemist spell fully works

--- Alchemist.lua
+++ Alchemist.lua
@@ -6,37 +6,61 @@ end
 
 function Alchemist:spell(targetCell)
         --lefele 1. mezo balra
-        if      (targetCell.x == self.x - 1 and targetCell.y == self.y - 1) or (targetCell.x == self.x - 2 and targetCell.y == self.y - 2) then
+    if  (self.x - 1 > 0 and self.x - 2 > 0) and (self.y - 1 > 0 and self.y - 2 > 0) and 
+        (targetCell.x == self.x - 1 and targetCell.y == self.y - 1) or 
+        (targetCell.x == self.x - 2 and targetCell.y == self.y - 2) then
+        
+            boardGrid[self.x - 1][self.y - 1].isPoisoned = true
+            boardGrid[self.x - 2][self.y - 2].isPoisoned = true
     
-                boardGrid[self.x - 1][self.y - 1].isPoisoned = true
-                boardGrid[self.x - 2][self.y - 2].isPoisoned = true
+    elseif (self.x - 1 > 0 and self.y - 1 > 0) and (targetCell.x == self.x - 1 and targetCell.y == self.y - 1) then
+            
+            boardGrid[self.x - 1][self.y - 1].isPoisoned = true
 
-        else self.isInSpellState = false
-        end
-        ---felfele 1. mezo
-        if      (targetCell.x == self.x + 1 and targetCell.y == self.y - 1) or (targetCell.x == self.x + 2 and targetCell.y == self.y - 2) then
-    
+    else self.isInSpellState = false
+    end
+    ---felfele 1. mezo
+    if  (self.x + 1 <= 10 and self.x + 2 <= 10) and (self.y - 1 > 0 and self.y - 2 > 0) and    
+        (targetCell.x == self.x + 1 and targetCell.y == self.y - 1) or 
+        (targetCell.x == self.x + 2 and targetCell.y == self.y - 2) then
+
             boardGrid[self.x + 1][self.y - 1].isPoisoned = true
             boardGrid[self.x + 2][self.y - 2].isPoisoned = true
 
-        else self.isInSpellState = false
-        end
+    elseif (self.x + 1 <= 10 and self.y - 1 > 0) and (targetCell.x == self.x + 1 and targetCell.y == self.y - 1) then
+            
+            boardGrid[self.x + 1][self.y - 1].isPoisoned = true
 
-        if      (targetCell.x == self.x - 1 and targetCell.y == self.y + 1) or (targetCell.x == self.x - 2 and targetCell.y == self.y + 2) then
-    
+    else self.isInSpellState = false
+    end
+
+    if  (self.x - 1 > 0 and self.x - 2 > 0) and (self.y + 1 <= 10 and self.y + 2 <= 10) and
+        (targetCell.x == self.x - 1 and targetCell.y == self.y + 1) or
+        (targetCell.x == self.x - 2 and targetCell.y == self.y + 2) then
+
             boardGrid[self.x - 1][self.y + 1].isPoisoned = true
             boardGrid[self.x - 2][self.y + 2].isPoisoned = true
 
-        else self.isInSpellState = false
-        end
+    elseif (self.x - 1 > 0 and self.y + 1 <= 10) and (targetCell.x == self.x - 1 and targetCell.y == self.y + 1) then
 
-        if      (targetCell.x == self.x + 1 and targetCell.y == self.y + 1) or (targetCell.x == self.x + 2 and targetCell.y == self.y + 2) then
+            boardGrid[self.x - 1][self.y + 1].isPoisoned = true
+
+    else self.isInSpellState = false
+    end
+
+    if  (self.x + 1 <= 10 and self.x + 2 <= 10) and (self.y + 1 <= 10 and self.y + 2 <= 10) and
+        (targetCell.x == self.x + 1 and targetCell.y == self.y + 1) or 
+        (targetCell.x == self.x + 2 and targetCell.y == self.y + 2) then
 
             boardGrid[self.x + 1][self.y + 1].isPoisoned = true
             boardGrid[self.x + 2][self.y + 2].isPoisoned = true
 
-        else self.isInSpellState = false
-        end
+    elseif (self.x + 1 <= 10 and self.y + 1 <= 10) and (targetCell.x == self.x + 1 and targetCell.y == self.y + 1) then
+            
+            boardGrid[self.x + 1][self.y + 1].isPoisoned = true
+
+    else self.isInSpellState = false
+    end
 
 end
 
