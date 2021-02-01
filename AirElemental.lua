0dbce66adc1bf430c6401c6cb8829e4ada9ec9ce
Kaosz Elemer <attila.k84@gmail.com>
2021-02-01 20:34:27 +0100

    bugfix - typo

--- AirElemental.lua
+++ AirElemental.lua
@@ -25,7 +25,7 @@ function AirElemental:spell(targetCell)
                 end
         
                 if (self.x - 2 > 0 and self.y - 2 > 0) and boardGrid[self.x - 1][self.y - 1].isOccupied and not boardGrid[self.x - 2][self.y - 2].occupiedBy then
-                        boardGrid[self.x - 1][self.y - 1].occupiedBy.stepPoints = oardGrid[self.x - 1][self.y - 1].occupiedBy.stepPoints + 1
+                        boardGrid[self.x - 1][self.y - 1].occupiedBy.stepPoints = boardGrid[self.x - 1][self.y - 1].occupiedBy.stepPoints + 1
                         boardGrid[self.x - 1][self.y - 1].occupiedBy:move(self.x - 2, self.y - 2)
                         
                 end
