154c88d8aa60a291abee2b9785a1e57ed6bdb44a
Kaosz Elemer <attila.k84@gmail.com>
2021-02-01 15:21:00 +0100

    defend works

--- Character.lua
+++ Character.lua
@@ -27,13 +27,13 @@ function Character:draw()
     end
     
     if self.isActionMenuDrawn == true then
-        love.graphics.draw(attackIcon, x, y)
-        love.graphics.draw(moveIcon, x + (tileW - tileW / 2), y)
-        love.graphics.draw(spellIcon, x, y + (tileH - tileH / 2))
-        love.graphics.draw(defenseIcon, x + (tileW - tileW / 2), y + (tileH - tileH / 2))     
+        if self.actionPoints ~= 0 then love.graphics.draw(attackIcon, x, y) end
+        if self.stepPoints ~= 0 then love.graphics.draw(moveIcon, x + (tileW - tileW / 2), y) end
+        if self.actionPoints ~= 0 then love.graphics.draw(spellIcon, x, y + (tileH - tileH / 2)) end
+        if self.actionPoints ~= 0 then love.graphics.draw(defenseIcon, x + (tileW - tileW / 2), y + (tileH - tileH / 2)) end   
     end
 
-    if self.isInStepState then 
+    if self.isInStepState and self.stepPoints ~= 0 then 
         if self.x + 1 < 11 and self.isWalkable[boardGrid[(self.x + 1)][self.y].class.name] and not boardGrid[(self.x + 1)][self.y].isOccupied then
             love.graphics.draw(validStepImage, (self.x + 1) * tileW + offsetX, self.y  * tileH + offsetY)
         end
@@ -63,7 +63,7 @@ function Character:draw()
 
 
 
-    if self.isInAttackState then
+    if self.isInAttackState and self.actionPoints ~= 0 then
         if self.x + 1 < 11 and boardGrid[(self.x + 1)][self.y].isOccupied and  boardGrid[(self.x + 1)][self.y].occupiedBy.parentPlayer ~= self.parentPlayer  then love.graphics.draw(validAttackImage, (self.x + 1) * tileW + offsetX, self.y  * tileH + offsetY) end
         if self.x - 1 > 0 and boardGrid[(self.x -1)][self.y].isOccupied and  boardGrid[(self.x - 1)][self.y].occupiedBy.parentPlayer ~= self.parentPlayer then love.graphics.draw(validAttackImage, (self.x - 1) * tileW + offsetX, self.y  * tileH + offsetY) end
         if self.y + 1 < 11 and boardGrid[self.x][(self.y + 1)].isOccupied and  boardGrid[(self.x)][self.y + 1].occupiedBy.parentPlayer ~= self.parentPlayer then love.graphics.draw(validAttackImage, self.x * tileW + offsetX, (self.y + 1)  * tileH + offsetY) end
@@ -74,7 +74,7 @@ function Character:draw()
         if self.x - 1 > 0 and self.y + 1 < 11 and boardGrid[(self.x - 1)][self.y + 1].isOccupied and  boardGrid[(self.x - 1)][self.y + 1].occupiedBy.parentPlayer ~= self.parentPlayer then love.graphics.draw(validAttackImage, (self.x - 1) * tileW + offsetX, (self.y + 1) * tileH + offsetY) end 
     end
 
-    if self.isInSpellState then
+    if self.isInSpellState and self.actionPoints ~= 0 then
     if self.id == 1 or self.id == 5 then
         if self.y + 1 < 11 then love.graphics.draw(validSpellImage, (self.x) * tileW + offsetX, (self.y + 1) * tileH + offsetY) end
         if self.y - 1 > 0 then love.graphics.draw(validSpellImage, (self.x) * tileW + offsetX, (self.y - 1) * tileH + offsetY) end
@@ -128,10 +128,13 @@ end
 
 function Character:click(mX, mY)
 
+    if selectedChar and selectedChar.isInDefenseState  then
+            local cell = boardGrid[self.x][ self.y]
+            selectedChar:defend(cell)
+    end
 
    
     if selectedChar and selectedChar.isInAttackState and selectedChar.parentPlayer ~= self.parentPlayer then
-        print (boardGrid[selectedChar.x], self.x)
         if  self.x + 1 <= 10 and boardGrid[selectedChar.x][selectedChar.y] == boardGrid[self.x + 1][self.y]  or
             self.x - 1 > 0 and boardGrid[selectedChar.x][selectedChar.y] == boardGrid[self.x - 1][self.y] or
             self.y + 1 <= 10 and boardGrid[selectedChar.x][selectedChar.y] == boardGrid[self.x][self.y + 1] or
@@ -185,22 +188,22 @@ function Character:chooseActionMenu(mx, my)
     local cy = self.y * tileH + offsetY
 
         -- BAl FELSO NEGYED - ATTACK STATE
-        if  mx > cx and mx < cx + tileW / 2 and  my > cy and my < cy + tileH / 2 then
+        if  mx > cx and mx < cx + tileW / 2 and  my > cy and my < cy + tileH / 2 and self.actionPoints ~= 0 then
                 self.isInAttackState = true
                 self.isActionMenuDrawn = false
         end
         -- JOBB FELSÖ NEGYED - STEP STATE                       
-        if  mx > cx + tileW / 2 and mx < cx + tileW and my > cy and my < cy + tileH / 2 then
+        if  mx > cx + tileW / 2 and mx < cx + tileW and my > cy and my < cy + tileH / 2 and self.stepPoints ~= 0 then
                 self.isInStepState = true
                 self.isActionMenuDrawn = false
         end
         -- BAL ALSO NEGYED - SPELL STATE
-        if   mx > cx and mx < cx + tileW / 2 and my > cy + (tileH / 2) and my < cy + tileH then
+        if   mx > cx and mx < cx + tileW / 2 and my > cy + (tileH / 2) and my < cy + tileH and self.actionPoints ~= 0 then
                 self.isInSpellState = true
                 self.isActionMenuDrawn = false     
         end
         -- JOBB ALSO NEGYED - DEFENSE STATE
-        if mx > cx + tileW / 2 and mx < cx + tileW and  my > cy + tileH / 2 and my < cy + tileH then
+        if mx > cx + tileW / 2 and mx < cx + tileW and  my > cy + tileH / 2 and my < cy + tileH and self.actionPoints ~= 0 then
                 self.isInDefenseState = true
                 self.isActionMenuDrawn = false
         end
@@ -231,17 +234,17 @@ function Character:move(x, y)
 end
 
 
-function Character:defend()
+function Character:defend(cell)
+    print("defending")
     if self.isInDefenseState and self.actionPoints ~= 0 and self.isHovered then
         
-        self.baseDefense = self.baseDefense + 2
+        cell.defenseModifier = cell.defenseModifier + 2
         self.actionPoints = 0
         self.stepPoints = 0
         self.drawActionMenu = false
-        self.isInDefenseState = false
         self.isSelected = false
     else self.isInDefenseState = false
-        end
+    end
 end
 
 function Character:kill()
