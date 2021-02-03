6dbcd2ea8890272a2f0f1872d8cd03e9c45dd00e
Kaosz Elemer <attila.k84@gmail.com>
2021-02-03 06:50:39 +0100

    working on events

--- classes/events/Event001.lua
+++ classes/events/Event001.lua
@@ -12,30 +12,8 @@ function Event001:init(x, y)
     )
 end
 
-function drawEventOnBoard()
 
-    love.graphics.draw(self.backgroundImage, width / 2 - offsetX / 2, height / 2 - offsetY / 2)
-    love.graphics.draw(self.image, (width / 2 - offsetX / 2) + 76, (height / 2 - offsetY / 2) + 110)
-    love.graphics.print(self.name, (width / 2 - offsetX / 2) + 56, (height / 2 - offsetY / 2) + 26)
-    love.graphics.print(self.id, (width / 2 - offsetX / 2) + 270, (height / 2 - offsetY / 2) + 26)
-    love.graphics.print(self.flavorText, (width / 2 - offsetX / 2) + 38, (height / 2 - offsetY / 2) + 276)
-    love.graphics.print(self.text, (width / 2 - offsetX / 2) + 166, (height / 2 - offsetY / 2) + 104 )
-
-end
-
-function confirmEventWithClick(mx, my)
-
-    if  mx > (width / 2 - offsetX / 2) + 302 and mx < (width / 2 - offsetX / 2) + 250 and
-            my > (height / 2 - offsetY / 2) + 310 and my < ((width / 2 - offsetX / 2) + 260) then
-       
-        self:burnAllCells()
-        isDrawEventOnBoard = false
-
-    end
-
-end
-
-local function burnAllCells()
+local function eventFunction()
 
     for x = 1, 10 do
         for y = 1, 10 do
